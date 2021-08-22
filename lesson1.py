7d7c3de716feaf7234a938efe16b9c88ff6dd9e4
Nrdleague <51178386+Nrdleague@users.noreply.github.com>
2021-08-22 14:26:41 -0400

    Update lesson1.py

--- lesson1.py
+++ lesson1.py
@@ -4,31 +4,41 @@
 
 
 # A string variable called favorite_car assigned whatever your favorite car is.
-
+favorite_car = ['honda civic ', 'datsun S30']
+    print(favorite_car)
 
 # A int variable called car_year with the year of your favorite vehicle
-
+car_year = ('1970')
+    print(car_year)
 
 # A float variable called pie that equals 3.14
-
+pie = 3.14
+    print(pie)
 
 # A list variable called asian_car_vendors that contain Honda, Toyota, Mitsubishi, and Kia
+asian_car_vendors = ['honda ', ' toyota ', ' Mitusubishi ', ' Kia ']
+
 
 # A dictionary variabled called car_types that will have the Honda, Ford, and Chevrolet as keys, you must provide a value for each. Example, { "Dodge" : "Ram"}
-
+car_typedict = ['Honda' : 'Civic'
+               'Ford': 'Focus ST'
+               'Chevorlet': 'Cruze'] 
 
 # A string variable called favorite_food assigned the name of your favorite food
-
-
+favorite_food = 'Pizza'
+    print(favorite_food)
 # A int variable called how_many that contains how many toes you have
-
+how_many = 10
 
 # A float variable called cost that equals the current price of gas
-
+cost = 3.0
+    print(cost)
 
 # A list variable called sodas that should contain at least 3 soda brands
-
+sodas = 'Sprite', 'Dr.pepper', 'Mountain Dew'
 
 # A dictionary variable called game_characters with the Nintendo, Sega, and Microsoft
 # as the keys. You will provide a list of at least 2 characters for each system
-
+game_characters = ['Nintendo' : 'Mario', 'bowser'
+                  'Sega': 'Kiryu', 'Majima'
+                  'Microsoft' : 'Master chief', 'lara croft']
